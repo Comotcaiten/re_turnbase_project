07bf8b33e8c1aea37f261296a371ba6a93277da2
Comotcaiten <pikachu123450vn@gmail.com>
2024-10-08 21:37:40 +0700

    change lb to lib

--- lib/3d/model/ModelBlockBench.gd
+++ lib/3d/model/ModelBlockBench.gd
@@ -6,6 +6,8 @@ class_name ModelBlockBench
 @onready var blockbench_export = $point/blockbench_export
 @onready var point = $point
 
+var child_model_point
+
 func _ready():
     if model == null or blockbench_export == null:
         print("[>] Model blockbench is Null")
@@ -13,6 +15,7 @@ func _ready():
         load_model(model)
 
 func load_model(_model: Resource):
+    # Init
     var instance = _model.instantiate()
     
     print("[>] Instance: ", instance)
@@ -21,13 +24,18 @@ func load_model(_model: Resource):
     if instance or blockbench_export:
         print("[>] Load: Start")
 
+        # Ready
         instance.position = blockbench_export.position
         instance.rotation = blockbench_export.rotation
 
+        # Replace
         point.add_child(instance)
         point.remove_child(blockbench_export)
+
+        # Save point
+        child_model_point = instance
 
         print("[>] Exist: ", blockbench_export)
         print("[>] Model loaded successfully.")
     else:
-        print("[>] Failed to load the model.")
+        print("[>] Failed to load the model. (Please check model and blockbench_export in point)")
