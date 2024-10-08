48aff12d48489c07f1b03f86163eea5048c7bd11
Comotcaiten <pikachu123450vn@gmail.com>
2024-10-08 21:34:25 +0700

    code a load Model 3d block bench

--- lb/3d/model/ModelBlockBench.gd
+++ lb/3d/model/ModelBlockBench.gd
@@ -1,11 +1,33 @@
 extends Node3D
 class_name ModelBlockBench
 
-@export var model: Texture3DRD
+@export var model: Resource # GLTF model resource
 
-@onready var op = $point/blockbench_export
+@onready var blockbench_export = $point/blockbench_export
+@onready var point = $point
 
-func _init():
-    if op == null:
+func _ready():
+    if model == null or blockbench_export == null:
         print("[>] Model blockbench is Null")
-    pass
+    else:
+        load_model(model)
+
+func load_model(_model: Resource):
+    var instance = _model.instantiate()
+    
+    print("[>] Instance: ", instance)
+    print("[>] Exist: ", blockbench_export)
+
+    if instance or blockbench_export:
+        print("[>] Load: Start")
+
+        instance.position = blockbench_export.position
+        instance.rotation = blockbench_export.rotation
+
+        point.add_child(instance)
+        point.remove_child(blockbench_export)
+
+        print("[>] Exist: ", blockbench_export)
+        print("[>] Model loaded successfully.")
+    else:
+        print("[>] Failed to load the model.")
