7fbf447619e8a9bf55b84397ac57751f2c01b1a1
Comotcaiten <pikachu123450vn@gmail.com>
2024-10-11 01:55:42 +0700

    add some model and test some code

--- lib/3d/index3D.gd
+++ lib/3d/index3D.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var player: BattleUnit
+@export var enemy: BattleUnit
 
 func _ready():
   set_up()
@@ -10,18 +11,32 @@ func _process(_delta: float):
   if Input.is_action_just_pressed("ui_skill_1"):
     print("[>] Action")
     player.RunAnimation3D("fight")
-  # if Input.is_action_just_pressed("ui_skill_2"):
-  #   print("[>] Action")
-  #   player.RunAnimation3D("fight2")
-  # if Input.is_action_just_pressed("ui_skill_3"):
-  #   print("[>] Action")
-  #   player.RunAnimation3D("fight3")
-  # if Input.is_action_just_pressed("ui_skill_4"):
-  #   print("[>] Action")
-  #   player.RunAnimation3D("fight4")
+    enemy.RunAnimation3D("fight")
+  if Input.is_action_just_pressed("ui_skill_2"):
+    print("[>] Action")
+    player.RunAnimation3D("fight2")
+    enemy.RunAnimation3D("fight")
+  if Input.is_action_just_pressed("ui_skill_3"):
+    print("[>] Action")
+    player.RunAnimation3D("fight3")
+    enemy.RunAnimation3D("fight")
+  if Input.is_action_just_pressed("ui_skill_4"):
+    print("[>] Action")
+    player.RunAnimation3D("fight4")
+    enemy.RunAnimation3D("fight")
   pass
 
 func set_up():
   player.SetUpOnlyCharacter()
   player.SetModel3D()
+
+  enemy.SetUpOnlyCharacter()
+  enemy.SetModel3D()
+
+  for unit in [player, enemy]:
+    is_player_unit_set_up(unit)
   print("[>] Player: ", player)
+
+func is_player_unit_set_up(_unit: BattleUnit):
+  if !_unit.isPlayerUnit:
+    _unit.rotation.y = deg_to_rad(180)
