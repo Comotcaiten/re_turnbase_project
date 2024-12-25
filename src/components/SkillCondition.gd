8db198cd72fe4efe7bb12cda2569f3be22878260
Comotcaiten <pikachu123450vn@gmail.com>
2024-12-26 00:27:52 +0700

    Rewrite, new version 0.5

--- src/components/SkillCondition.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name SkillCondition
-
-
-func is_condition_met(_targets: Array[UnitModel], _source: UnitModel, _skill: SkillModel, _battle_system: BattleSystemController) -> bool:
-  return true
--- src/components/SkillMechanic.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name SkillMechanic
-
-func apply_mechanic(_targets: Array[UnitModel], _source: UnitModel, _skill: SkillModel, _battle_system: BattleSystemController) -> bool:
-  return true
