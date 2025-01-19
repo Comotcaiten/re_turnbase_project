45df714c2b578977b47ee137e0e03829592aa7d2
Comotcaiten <pikachu123450vn@gmail.com>
2025-01-19 22:48:39 +0700

    Start new version, Version 0.6

--- src/components/SkillCondition.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-
-class_name SkillCondition
-
-func check(_targets: Array[UnitModel] = [], _source: UnitModel = null, _skill: SkillModel = null, _battle_system: BattleSystemController = null) -> bool:
-  return true
--- src/components/SkillMechanic.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-
-class_name SkillMechanic
-
-func apply_mechanic(_targets: Array[UnitModel] = [], _source: UnitModel = null, _skill: SkillModel = null, _battle_system: BattleSystemController = null) -> bool:
-  return true
